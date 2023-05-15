d50369c92feedfa5d6de8c59c0aa74eafe0c8a90
DavidNguyen <davidGit@gmx.de>
2023-05-15 14:22:05 +0200

    fixed TTS bug #18

--- discord_bot/TTS/DaiblVoice.py
+++ discord_bot/TTS/DaiblVoice.py
@@ -12,14 +12,15 @@ class Voice:
         return audio.info.length
     
     async def tts(self, voiceChannel, text):
+        cwd = os.path.dirname(os.path.abspath(__file__))
         speech = gTTS(text = text, slow=False)
-        speech.save("discord_bot\\TTS\\answer.mp3")
+        speech.save(cwd + "\\answer.mp3")
         
         vc = await voiceChannel.connect()
-        vc.play(discord.FFmpegPCMAudio(executable="discord_bot\\TTS\\ffmpeg-6.0-full_build\\bin\\ffmpeg.exe" , source="answer.mp3"), after = lambda e: print("done"))
+        vc.play(discord.FFmpegPCMAudio(executable= cwd + "\\ffmpeg-6.0-full_build\\bin\\ffmpeg.exe" , source= cwd + "\\answer.mp3"), after = lambda e: print("done"))
         
         counter = 0
-        cwd = os.getcwd()
+        
         duration = self.audio_len(cwd + "\\answer.mp3")
         while not counter >= duration:
             await asyncio.sleep(1)
