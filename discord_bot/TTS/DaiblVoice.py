1d8a6d7406810bf1883ee41f966c306ff1a56084
neumannvi84434 <neumanvi84434@th-nuernberg.de>
2023-05-19 13:39:56 +0200

    refactored on_message Contributors: @huberpa85956 @nguyenda81452

--- discord_bot/TTS/DaiblVoice.py
+++ discord_bot/TTS/DaiblVoice.py
@@ -7,25 +7,13 @@ from mutagen.mp3 import MP3
 
 class Voice:
     
-    def audio_len(self, path):
-        audio = MP3(path)
-        return audio.info.length
-    
-    async def tts(self, voiceChannel, text):
+    async def TTS(self, voiceChannel, text):
         cwd = os.path.dirname(os.path.abspath(__file__))
         speech = gTTS(text = text, slow=False)
         speech.save(cwd + "\\answer.mp3")
         
-        vc = await voiceChannel.connect()
-        vc.play(discord.FFmpegPCMAudio(executable= cwd + "\\ffmpeg-6.0-full_build\\bin\\ffmpeg.exe" , source= cwd + "\\answer.mp3"), after = lambda e: print("done"))
+        voiceChannel.play(discord.FFmpegPCMAudio(executable= cwd + "\\ffmpeg-6.0-full_build\\bin\\ffmpeg.exe" , source= cwd + "\\answer.mp3"), after = lambda e: print("done"))
         
-        counter = 0
-        
-        duration = self.audio_len(cwd + "\\answer.mp3")
-        while not counter >= duration:
-            await asyncio.sleep(1)
-            counter += 1
-        await vc.disconnect()
     
     
     
--- discord_bot/main.py
+++ discord_bot/main.py
@@ -11,42 +11,23 @@ from Bot.Daibl import Daibl
 # credential stored in environment variables (should be locally on every machine) 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+GUILD = int(os.getenv('DISCORD_GUILD'))
 MODEL = os.getenv('MODEL_PATH')
 
 # Client setup
 ## Client is an object that represents a connection to Discord
 intents = discord.Intents.default()
 intents.message_content = True
-""" client = discord.Client(intents=intents) """
-
-bot = Daibl(intents = intents, guild_id= 1085963828589756437, command_prefix="#daibl")
 
 modelCommunicatior = ModelCommunicator(MODEL)
 voice = Voice()
+bot = Daibl(
+    intents = intents, 
+    guild_id= GUILD, 
+    modelCommunicatior = modelCommunicatior, 
+    voice = voice ,
+    command_prefix="#daibl"
+    )
 
-""" @client.event
-async def on_message(message):
-    #checks message in every channel
-    if message.author == client.user:
-        return
-    #all infos of message (channel, author, ...)
-    print(message)
-    
-    if message.content.startswith("#daibl"):
-        answer = communicateWithModel(message.content.replace("#daibl ", ""))
-        
-        await message.channel.send(answer)
-        await voice.tts(message.author.voice.channel , answer)
-        #tts = gtts.gTTS(answer)
-        #tts.save("discord_bot\\TTS\\answer.mp3")
-        #playsound("discord_bot\\TTS\\answer.mp3") """
-
-
-
-def communicateWithModel(message):
-    promptResult = modelCommunicatior.returnPromptText(message)
-    return promptResult
-
-# starting the client (-> last line of file)
+# starting the bot
 bot.run(TOKEN)
