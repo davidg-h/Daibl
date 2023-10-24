95790bb36d2d30b69b0c7596923c3074d799ee30
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2023-10-24 19:07:06 +0200

    deleted old path

--- discord_bot/scrap/query_crafter.py
+++ discord_bot/scrap/query_crafter.py
@@ -5,7 +5,7 @@ import pandas as pd
 from scipy.spatial.distance import cosine
 from scrap.question_embedding import get_5_most_similar_documents
 from scrap.db_init import db_get_df
-from scrap.spacy_keywordextraction import extraction
+# from scrap.spacy_keywordextraction import extraction
 import json
 
 DATABASE_PATH = "discord_bot/scrap/html.sqlite"
