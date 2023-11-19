e3016213db89a480cec61e7d4e8f8ce1f344c2fc
nguyenda81452 <nguyenda81452@th-nuernberg.de>
2023-11-19 22:16:22 +0100

    another llm test

--- discord_bot/test/Test_LLM/llm3.py
+++ discord_bot/test/Test_LLM/llm3.py
@@ -18,7 +18,7 @@ login(token=hf_token)
 
 prompt = "Wer war Napoleon?"
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-model_id = "jphme/Llama-2-13b-chat-german"
+model_id = "jphme/em_german_7b_v01"
 
 #llm = AutoModelForCausalLM.from_pretrained(model_id, model_file="llama-2-13b-german-assistant-v2.Q4_K_M.gguf", model_type="llama", gpu_layers=50, hf=True)
 #print(llm(prompt))
@@ -30,17 +30,16 @@ generator = pipeline(
     #tokenizer=tokenizer, 
     torch_dtype=torch.float16, 
     device_map="auto",
-    temperature=0.8,
+    temperature=0.7,
     top_p=0.15,
-    top_k=10,
+    top_k=15,
     repetition_penalty=1.1,
     num_return_sequences=1,
     #eos_token_id=tokenizer.eos_token_id,
     max_new_tokens=50,
-    do_sample=True,
     #max_length=256,
     )
-answer = generator(prompt)
+answer = generator(prompt, do_sample=True)
 print(answer[0]['generated_text'])
 
 logout()
