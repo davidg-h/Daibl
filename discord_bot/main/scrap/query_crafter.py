2718dcd99b230e7e4450f43ad870da6196f7f4dd
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2023-12-02 21:25:49 +0100

    added a query for all different approaches

--- discord_bot/main/scrap/query_crafter.py
+++ discord_bot/main/scrap/query_crafter.py
@@ -7,8 +7,8 @@ from scipy.spatial.distance import cosine
 from scrap.db_init import db_get_df
 #from scrap.spacy_keywordextraction import extraction
 import json
-from scrap.question_embedding_MiniLM import get_most_similar_articles
-from scrap.msmarco_roberta_base_v3 import get_most_similar_articles_msmarco
+from scrap.embedding_algorithms.question_embedding_MiniLM import get_most_similar_articles
+from scrap.embedding_algorithms.msmarco_roberta_base_v3 import get_most_similar_articles_msmarco
 
 
 # best 5 documents as context
@@ -28,25 +28,34 @@ from scrap.msmarco_roberta_base_v3 import get_most_similar_articles_msmarco
 #     print(query)
 #     return query
 
+def construct_query(documents, message):
+    query=f"""
+        {'----document----'.join(documents)}
+        \n----document----\n
+        in regard of the documents above, anwser the following question: \n
+        {message.replace('$daibl ', '')}
+        """
+
+    print(query)
+    return query
+    
+
 # all documents as context
 # throws error on api
 def get_query_all(message):
     df = db_get_df("word_embeddings", ["text"])
     documents = df["text"]
-    query="\n----Dokument----\n".join(documents)+"\n"+"in regard of the documents above,anwser the following question: \n"+ message.replace("$daibl ", "")
-    print(query)
+    query = construct_query(documents, message)
     return query
 
 # get 5 best articles with MiniLM-L6-v2
 def get_query_embeddings_MiniLM(message):
-    best_documents = get_most_similar_articles(message,5)
-    query="\n----Dokument----\n".join(best_documents)+ "\n"+"Bezüglich der oben genannten Dokumente, beantworte die folgende Frage: \n"+ message.replace("$daibl ", "")
-    print(query)
+    documents = get_most_similar_articles(message,5)
+    query = construct_query(documents, message)   
     return query
 
 # get 5 best articles with embeddings_msmarco_distilroberta_base_v3
 def get_query_embeddings_Msmarco(message):
-    best_documents = get_most_similar_articles_msmarco(message,5)
-    query="\n----Dokument----\n".join(best_documents)+ "\n"+"Bezüglich der oben genannten Dokumente, beantworte die folgende Frage: \n"+ message.replace("$daibl ", "")
-    print(query)
+    documents = get_most_similar_articles_msmarco(message,5)
+    query = construct_query(documents, message) 
     return query
